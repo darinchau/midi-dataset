4f303fb82ce01c9ebf9e62c93aa4e19b631e0378
Darin Chau <darinchauyf@gmail.com>
2025-05-09 21:12:32 +0800

    temporarily delete all analysis functions

--- main.py
+++ main.py
@@ -9,51 +9,7 @@ import time
 T = typing.TypeVar("T")
 
 
-def instruments(midi_path: str) -> list[int]:
-    try:
-        mid = mido.MidiFile(midi_path)
-        instrument_set = []
-
-        for track in mid.tracks:
-            for msg in track:
-                if msg.type == 'program_change':
-                    instrument_name = msg.program
-                    instrument_set.append(instrument_name)
-        return instrument_set
-    except Exception as e:
-        return []
-
-
-def length(file_path):
-    vlc_options = [
-        f"--soundfont=./resources/gs.sf2",
-        "--no-audio",
-        "--quiet"
-    ]
-
-    instance = vlc.Instance(vlc_options)
-    player = instance.media_player_new()  # type: ignore
-    media = instance.media_new(file_path)  # type: ignore
-    player.set_media(media)
-    player.play()
-
-    for i in range(100):
-        duration = player.get_length()
-        if duration <= 1e-5:
-            time.sleep(0.1)
-        else:
-            break
-    else:
-        return -1
-    player.stop()
-    player.release()
-
-    if duration <= 1e-5:
-        return -1
-    return duration / 1000.0
-
-
-def analyse(f: typing.Callable[[str], T], name: str | None = None) -> None:
+def analyse(f: typing.Callable[[str], T], name: str | None = None, num_threads: int = 16) -> None:
     """Analyse the dataset with a function and save the result to a json file.
 
     The name of the json file will be the name of the function by default"""
@@ -70,7 +26,7 @@ def analyse(f: typing.Callable[[str], T], name: str | None = None) -> None:
 
     result = ds.accumulate(
         f,
-        num_threads=16,
+        num_threads=num_threads,
     )
     # Save result to a json file
     with open(path, "w") as file:
@@ -79,4 +35,8 @@ def analyse(f: typing.Callable[[str], T], name: str | None = None) -> None:
 
 
 if __name__ == "__main__":
-    analyse(length)
+    import sys
+    fn = sys.argv[1]
+    f = vars()[fn]
+    num_threads = int(sys.argv[2]) if len(sys.argv) > 2 else 16
+    analyse(f, num_threads=num_threads)
